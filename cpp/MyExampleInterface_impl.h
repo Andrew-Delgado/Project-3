804ae8eca43a2b6ebef7e82a6e50439ebce8e51e
Andrew Delgado <44880555+Andrew-Delgado@users.noreply.github.com>
2019-03-14 13:21:47 -0700

    Update MyExampleInterface_impl.h

--- cpp/MyExampleInterface_impl.h
+++ cpp/MyExampleInterface_impl.h
@@ -2,11 +2,18 @@
 #define __MY_EXAMPLE_INTERFACE_IMPL_H__
 
 #include "example.hh"
+#include <vector>
+#include <string>
 
 class MyExampleInterface_impl : public POA_ExampleInterface
 {
 	public:
+		///////////vector<string> questions;
+		//vector<string> answers;
+		inline MyExampleInterface_impl(){}
 		virtual char * send_message(const char * message);
+		virtual void createQA(const char * q, const char * a);
+
 };
 
 #endif // __MY_EXAMPLE_INTERFACE_IMPL_H__
